25dfeb68f6fb244645640234376ed7b7c6c70183
CongTrinhh <nguyencongtrinhqb@gmail.com>
2023-09-11 11:32:51 +0700

    update env

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,6 @@ const router = express.Router();
 const AuthController = require('../controllers/auth.Controller');
 const bodyParser = require('body-parser')
 router.use(bodyParser.json())
-// const connection = require('../database/connection');
-// const { hashPasswordWithSalt } = require('../modules/auth/hash');
-// const { signToken,authorize } = require('../modules/auth/jwt');
-// const {validateRegisterRequest,validateLoginRequest} = require('../middleware/validate')
-// const { mailServices } = require('../services/mail.services');
-// const TokenService = require('../services/token.service');
-// const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
 // router.post('/login',[validateLoginRequest], (req, res,next) => {
 // 	connection.query('select * from user where email = ?',[req.body.email],(err,result,field)=>{
 // 		if(result.length == 1){
@@ -29,19 +21,6 @@ router.post('/login',AuthController.login)
 router.post('/register',AuthController.register)
 router.put('/:id',AuthController.updateUserInfo)
 router.delete('/:id',AuthController.deleteUser)
-// router.post('/register',[validateRegisterRequest], (req, res,next) => {
-// 	let user = req.body;
-// 	let hashed = hashPasswordWithSalt(user.password,false);
-// 	user.password = hashed.password;
-// 	user.salt = hashed.salt;
-// 	console.log(user);
-// 	connection.query(`insert into user (name,email,password,salt) value (?,?,?,?)`,[user.name, user.email, user.password,user.salt], (error, results, fields) => {
-// 			console.log(error,results);
-// 			if(!error){
-// 				res.send("Success")
-// 			}
-// 	})
-// })
 // router.post('/forgot-password',async (req, res,next) => {
 // 	try{
 // 		await mailServices.sendMail({
